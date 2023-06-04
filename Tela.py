00ad42ccb080812e2f33051ff90f0756ca88222b
MarcosAniCury <curymarcos@live.com>
2023-06-04 16:03:07 -0300

    Fixed bug windowing and zoom

--- Tela.py
+++ Tela.py
@@ -4,32 +4,30 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import ImageTk, Image
 
-def atualizar_imagem():
-    global imagem_atual, imagem_atual_photo
+def get_image_explorer():
+    global imagem_original, imagem_original_photo
 
     # Carregar a imagem atual
-    imagem_atual = Image.open(imagens[indice_imagem_atual])
-    imagem_atual_gray = imagem_atual.convert('L')  # converte para escala de cinza
-    imagem_atual_resize = imagem_atual_gray.resize((300, 400), Image.LANCZOS)
-    imagem_atual_photo = ImageTk.PhotoImage(imagem_atual_resize)
+    imagem_original = Image.open(imagens[indice_imagem_original])
+    imagem_original_gray = imagem_original.convert('L')  # converte para escala de cinza
+    imagem_original_resize = imagem_original_gray.resize((300, 400), Image.LANCZOS)
+    imagem_original_photo = ImageTk.PhotoImage(imagem_original_resize)
 
     # Atualizar o Label com a nova imagem
-    label_imagem.configure(image=imagem_atual_photo)
-    label_imagem.image = imagem_atual_photo  # Atualiza a referência da imagem no Label]
+    label_imagem.configure(image=imagem_original_photo)
+    label_imagem.image = imagem_original_photo  # Atualiza a referência da imagem no Label]
     
 def abrir_explorador():
     # Abrir o explorador de arquivos
     initial_path = "C:\\Users\\letic\\Documents\\GitHub\\Analise-de-mama\\mamografias"
     filename = filedialog.askopenfilename(initialdir=initial_path, title="Selecione uma imagem", filetypes=(("Arquivos de imagem", "*.jpg;*.jpeg;*.png"), ("Todos os arquivos", "*.*")))
-    # Carregar a imagem selecionada
-    imagem = Image.open(filename)
 
     if filename:
         # Atualizar a lista de imagens com a nova imagem selecionada
-        imagens[indice_imagem_atual] = filename
+        imagens[indice_imagem_original] = filename
 
         # Atualizar a imagem exibida
-        atualizar_imagem()
+        get_image_explorer()
 
 
 def trocar_imagem():
@@ -43,20 +41,22 @@ def trocar_imagem():
         indice_imagem = 0
 
     # atualiza a imagem exibida no Label
-    imagem_atual = Image.open("mamografias\DleftCC\\" + imagens[indice_imagem] )
-    imagem_atual_gray = imagem_atual.convert('L')  # converte para escala de cinza
-    imagem_atual_resize = imagem_atual_gray.resize((300, 400), Image.LANCZOS)
-    imagem_atual_photo = ImageTk.PhotoImage(imagem_atual_resize)
-    label_imagem.configure(image=imagem_atual_photo)
-    label_imagem.image = imagem_atual
+    imagem_original = Image.open("mamografias\DleftCC\\" + imagens[indice_imagem] )
+    imagem_original_gray = imagem_original.convert('L')  # converte para escala de cinza
+    imagem_original_resize = imagem_original_gray.resize((300, 400), Image.LANCZOS)
+    imagem_original_photo = ImageTk.PhotoImage(imagem_original_resize)
+    label_imagem.configure(image=imagem_original_photo)
+    label_imagem.image = imagem_original
 
-def update_image(*args):
+def update_image_windowing(*args):
+    global imagem_atual_resize, imagem_atual_photo, n_zooms
+
     # Obtém os valores da barra deslizante
     min_value = min_slider.get()
     max_value = max_slider.get()
 
     # Aplica o janelamento
-    imagem_ndarray = np.asanyarray(imagem_atual_gray)
+    imagem_ndarray = np.asanyarray(imagem_original_gray)
     output_img = np.zeros_like(imagem_ndarray)
     output_img = np.where(imagem_ndarray <= min_value, 0, output_img)
     output_img = np.where(imagem_ndarray > max_value, 255, output_img)
@@ -66,15 +66,29 @@ def update_image(*args):
 
 
     # Converte a imagem para o formato PIL para mostrar na janela
-    output_img_pil = Image.fromarray(output_img).resize((300, 400), Image.LANCZOS)
-    output_img_tk = ImageTk.PhotoImage(output_img_pil)
+    imagem_atual_resize = Image.fromarray(output_img).resize((300, 400), Image.LANCZOS)
+    imagem_atual_photo = ImageTk.PhotoImage(imagem_atual_resize)
 
     # Atualiza a imagem na janela
-    label_imagem.configure(image=output_img_tk)
-    label_imagem.image = output_img_tk
+    label_imagem.configure(image=imagem_atual_photo)
+    label_imagem.image = imagem_atual_photo
+
+    n_zooms_temp = n_zooms
+
+    if n_zooms > 0:
+        type_zoom = zoom_in
+    else: 
+        type_zoom = zoom_out
+    
+    n_zooms = 0
+
+    for i in range(abs(n_zooms_temp)):
+        type_zoom()
 
 def zoom_in():
-    global imagem_atual_resize, imagem_atual_photo
+    global imagem_atual_resize, imagem_atual_photo, n_zooms
+
+    n_zooms =  n_zooms + 1
 
     # Obtém as dimensões atuais da imagem redimensionada
     width, height = imagem_atual_resize.size
@@ -92,7 +106,9 @@ def zoom_in():
     label_imagem.image = imagem_atual_photo
 
 def zoom_out():
-    global imagem_atual_resize, imagem_atual_photo
+    global imagem_atual_resize, imagem_atual_photo, n_zooms
+
+    n_zooms = n_zooms - 1
 
     # Obtém as dimensões atuais da imagem redimensionada
     width, height = imagem_atual_resize.size
@@ -123,21 +139,26 @@ i = 0
 for i in range(1,315):
     imagens.append("d_left_cc (" + str(i) + ").png")
 
-indice_imagem_atual = 0
+indice_imagem_original = 0
 
 # carrega a imagem
-imagem_atual = Image.open("mamografias\DleftCC\d_left_cc (1).png")
-imagem_atual_gray = imagem_atual.convert('L')  # converte para escala de cinza
-imagem_atual_resize = imagem_atual_gray.resize((300, 400), Image.LANCZOS)
-imagem_atual_photo = ImageTk.PhotoImage(imagem_atual_resize)
+imagem_original = Image.open("mamografias\DleftCC\d_left_cc (1).png")
+imagem_original_gray = imagem_original.convert('L')  # converte para escala de cinza
+imagem_original_resize = imagem_original_gray.resize((300, 400), Image.LANCZOS)
+imagem_original_photo = ImageTk.PhotoImage(imagem_original_resize)
+
+imagem_atual_resize = imagem_original_resize
+imagem_atual_photo = imagem_original_photo
+
+n_zooms = 0
 
 # Cria um widget Label e exibe a imagem na janela
 label_imagem = Label(janela, width=800, height=600)
 label_imagem.grid(row=0, column=0, padx=10, pady=10)
 
 # Atualizar o Label com a imagem atual
-label_imagem.configure(image=imagem_atual_photo)
-label_imagem.image = imagem_atual_photo  # Atualiza a referência da imagem no Label
+label_imagem.configure(image=imagem_original_photo)
+label_imagem.image = imagem_original_photo  # Atualiza a referência da imagem no Label
 
 # Cria um frame para os botões
 frame_controles = Frame(janela)
@@ -147,17 +168,20 @@ frame_controles.grid(row=1, column=0, padx=10, pady=10)
 botao_selecionar = Button(frame_controles, text="Trocar Imagem", command=abrir_explorador)
 botao_selecionar.grid(row=0, column=0, padx=5)
 
-min_slider = Scale(frame_controles, from_=0, to=255, orient='horizontal', label='Valor mínimo', command=update_image)
+min_slider = Scale(frame_controles, from_=1, to=255, orient='horizontal', label='Valor mínimo')
 min_slider.grid(row=0, column=1, padx=5)
 
-max_slider = Scale(frame_controles, from_=0, to=255, orient='horizontal', label='Valor máximo', command=update_image)
+max_slider = Scale(frame_controles, from_=1, to=255, orient='horizontal', label='Valor máximo')
 max_slider.grid(row=0, column=2, padx=5)
 
-botao_zoom_in = Button(frame_controles, text="Zoom In", command=zoom_in)
+botao_zoom_in = Button(frame_controles, text="Janelamento", command=update_image_windowing)
 botao_zoom_in.grid(row=0, column=3, padx=5)
 
+botao_zoom_in = Button(frame_controles, text="Zoom In", command=zoom_in)
+botao_zoom_in.grid(row=0, column=4, padx=5)
+
 botao_zoom_out = Button(frame_controles, text="Zoom Out", command=zoom_out)
-botao_zoom_out.grid(row=0, column=4, padx=5)
+botao_zoom_out.grid(row=0, column=5, padx=5)
 
 # Configura a coluna 0 do grid para expandir
 janela.grid_columnconfigure(0, weight=1)
