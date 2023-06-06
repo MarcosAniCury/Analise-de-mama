3808765bacbe3f85a5e3075c6f7b596bf701a30f
Ludmila Bruna Santos Nascimento <ludbruna2009@hotmail.com>
2023-06-06 08:53:14 -0300

    ler diretório

---
+++ diretorio.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+# Caminhos dos diretórios para as imagens de treino e teste
+train_dir = "treino"
+test_dir = "teste"
+
+# Função para copiar uma imagem para o diretório de destino
+def copiar_imagem(origem, destino):
+    os.makedirs(destino, exist_ok=True)
+    shutil.copy2(origem, destino)
+
+# Diretório principal onde estão localizadas as imagens de mamografias
+mamografias_dir = "mamografias"
+
+# Percorrer todas as pastas dentro do diretório de mamografias
+for dirpath, dirnames, filenames in os.walk(mamografias_dir):
+    for arquivo in filenames:
+        if arquivo.endswith(".png") or arquivo.endswith(".jpg") or arquivo.endswith(".jpeg"):
+            nome_arquivo, extensao = os.path.splitext(arquivo)
+            numero = nome_arquivo.split("(")[-1].split(")")[0]  # Extrair o número entre parênteses
+            if numero.isdigit():
+                numero = int(numero)
+                imagem_origem = os.path.join(dirpath, arquivo)
+                if numero % 4 == 0:
+                    copiar_imagem(imagem_origem, test_dir)
+                else:
+                    copiar_imagem(imagem_origem, train_dir)
